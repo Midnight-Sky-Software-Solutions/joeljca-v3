6e9f4173805977ab2857df3047153fece6d858b9
Alex Johnston <me@alexj.ca>
2025-08-25 12:26:11 -0600

    Work on nav bar

--- ui/navbar.tsx
+++ ui/navbar.tsx
@@ -1,48 +1,79 @@
 import Image from "next/image";
+import Link from "next/link";
 
 export default function NavBar() {
   return (
     <header className="flex justify-center drop-shadow-md	bg-white">
       <div className="flex max-w-6xl grow h-20 items-center px-3">
-        <div className={`font-mono text-3xl`}>
+        <Link href="/" className={`font-mono text-3xl`}>
           Joel Johnston&nbsp;
             <span aria-hidden="true">[</span>
-            Web Dev
+            <span className="text-cyan-500">Web Dev</span>
             <span aria-hidden="true">]</span>
-        </div>
+        </Link>
         <nav className="grow flex justify-center">
           <ul className="flex gap-8 text-2xl">
-            <li>Services</li>
-            <li>Portfolio</li>
-            <li>Contact</li>
-            <li>Blog</li>
+            <li>
+              <Link href="/" className="text-blue-800">
+                Services
+              </Link>
+            </li>
+            <li>
+              <Link href="/" className="text-blue-800">
+                Portfolio
+              </Link>
+            </li>
+            <li>
+              <Link href="/" className="text-blue-800">
+                Contact
+              </Link>
+            </li>
+            <li>
+              <Link href="/" className="text-blue-800">
+                Blog
+              </Link>
+            </li>
           </ul>
         </nav>
         <div>
           <ul className="flex gap-3">
             <li>
-              <Image
-                src="/Upwork.svg"
-                width={45}
-                height={45}
-                alt="Upwork"
-              />
+              <Link 
+                href="https://www.upwork.com/freelancers/~011f757c30fa5d1800"
+                target="_blank"
+              >
+                <Image
+                  src="/Upwork.svg"
+                  width={45}
+                  height={45}
+                  alt="Upwork"
+                />
+              </Link>
             </li>
             <li>
-              <Image
-                src="/LinkedIn.svg"
-                width={45}
-                height={45}
-                alt="Upwork"
-              />
+              <Link
+                href="https://www.linkedin.com/in/joel-alexander-johnston/"
+                target="_blank"
+              >
+                <Image
+                  src="/LinkedIn.svg"
+                  width={45}
+                  height={45}
+                  alt="Upwork"
+                />
+              </Link>
             </li>
             <li>
-              <Image
-                src="/eMail.svg"
-                width={45}
-                height={45}
-                alt="Upwork"
-              />
+              <Link
+                href="mailto:me@joelj.ca"
+              >
+                <Image
+                  src="/eMail.svg"
+                  width={45}
+                  height={45}
+                  alt="Upwork"
+                />
+              </Link>
             </li>
           </ul>
         </div>
