2c0591e8535789b3f16f25708ac39529ffdf7589
Alex Johnston <me@alexj.ca>
2025-08-25 16:24:47 -0600

    Visit button

--- app/portfolio.tsx
+++ app/portfolio.tsx
@@ -1,4 +1,5 @@
 import Image from "next/image"
+import Link from "next/link";
 
 export default function Portfolio() {
   return (
@@ -10,9 +11,15 @@ export default function Portfolio() {
         <div className="flex">
           <div className="grow ">
             <h3 className="text-3xl pb-8">The Association of Home Pizza Cooks</h3>
-            <p className="text-2xl">
+            <p className="text-2xl pb-8">
               An association website built with React and Wild Apricot. This Proof of Concept demonstrate a Next.js application integrating with WildApricot and WordPress by consuming their respective Web APIs. The major technical challenge was implementing Single Sign On with WildApricot.
             </p>
+            <Link
+              href=""
+              className="bg-cyan-500 text-white text-2xl px-3 py-2 rounded-lg"
+            >
+              Visit
+            </Link>
           </div>
           <div className="w-128 flex-none">
             <Image
