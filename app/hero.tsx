239a4b75e84a3040a7de4b08688e8085357b8d32
Alex Johnston <me@alexj.ca>
2025-08-26 13:16:48 -0600

    Fix adaptive design bugs

--- app/hero.tsx
+++ app/hero.tsx
@@ -5,7 +5,7 @@ export default function Hero() {
     <div
       className="bg-linear-to-b from-cyan-800 to-cyan-shade flex justify-center"
     >
-      <div className="flex flex-col max-w-6xl grow items-center px-16 lg:px-32 lg:flex-row pt-12 lg:pt-0">
+      <div className="flex flex-col max-w-6xl grow items-center px-8 sm:px-16 lg:px-32 lg:flex-row pt-12 lg:pt-0">
         <div className="grow">
           <h2 className="text-white text-3xl font-serif text-center">Hi, I'm Joel 👋</h2>
           <p className="text-white text-2xl mt-10">
@@ -18,7 +18,7 @@ export default function Hero() {
             alt="A picture of me (Joel)"
             width={327}
             height={327}
-            className="translate-y-1/4 rounded-full border-5 border-teal-700"
+            className="translate-y-1/8 sm:translate-y-1/4 rounded-full border-5 border-teal-700 mx-3"
           />
         </div>
       </div>
