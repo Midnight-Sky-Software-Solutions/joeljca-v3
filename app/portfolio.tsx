09dc7a156cfd4d31e89b2ddde2a4c8c11439d079
Alex Johnston <me@alexj.ca>
2025-09-14 14:28:20 -0600

    Add new portfolio section

--- app/portfolio.tsx
+++ app/portfolio.tsx
@@ -29,17 +29,42 @@ export default function Portfolio() {
                 Case Study
               </Link>
             </div>
-            
-
           </div>
           <div className="lg:w-128 flex-none">
             <Image
               src="/mockuuups-macbook-pro-mockup.jpg"
               alt="A computer and a headset"
+              width={8256}
+              height={6192}
+              className="lg:float-right rounded-lg"
+            />
+          </div>
+        </div>
+        <div className="flex flex-col lg:flex-row gap-8 lg:gap-5 mt-12">
+          <div className="lg:w-128 flex-none">
+            <Image
+              src="/mockuuups-satp.jpg"
+              alt="A computer and a headset"
               width={1500}
               height={2000}
               className="lg:float-right rounded-lg"
             />
+          </div>
+          <div className="grow ">
+            <h3 className="text-3xl pb-8 font-serif">The Society of Association Technology Professionals</h3>
+            <p className="text-xl pb-8">
+              This demo project demonstrates how a powerful association website can be built without an AMS. In this proof of concept, I integrate Auth0, HubSpot, Contentful and Stripe to build an engaging web experience 
+              that serves both members and the public.
+            </p>
+            <div className="flex gap-3">
+              <Link
+                href="https://demo2.joelj.ca"
+                target="_blank"
+                className="bg-cyan-500 hover:bg-cyan-400 text-white text-xl px-3 py-2 rounded-lg"
+              >
+                Visit
+              </Link>
+            </div>
           </div>
         </div>
       </div>
