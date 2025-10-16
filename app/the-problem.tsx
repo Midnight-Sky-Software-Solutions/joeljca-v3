d33b737ae193c9a25f5fec4457c458b556b10a37
Alex Johnston <me@alexj.ca>
2025-10-16 14:53:11 -0600

    Fixed height hero

--- app/the-problem.tsx
+++ app/the-problem.tsx
@@ -2,12 +2,12 @@ import Image from "next/image";
 
 export default function TheProblem() {
   return (
-    <div className="full-width-container bg-[url(/stressed_worker.jpg)] bg-center bg-cover bg-fixed h-[50vh]">
+    <div className="full-width-container bg-[url(/stressed_worker.jpg)] bg-center bg-cover bg-fixed h-60">
       <div className="bg-black/60 grow flex flex-col items-center justify-center gap-2">
         <h2 className="text-white text-center">
           Frustrated with your legacy .NET project?
         </h2>
-        <p className="text-white text-2xl">
+        <p className="text-white text-2xl text-center">
           Don&apos;t let outdated software get in the way of your business goals.
         </p>
       </div>
