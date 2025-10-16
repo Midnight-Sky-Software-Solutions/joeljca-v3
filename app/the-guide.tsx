a1d06f6f176112f90015c5824e10ce7098fa8bb0
Alex Johnston <me@alexj.ca>
2025-10-16 17:56:40 -0600

    Dope checkmark

--- app/the-guide.tsx
+++ app/the-guide.tsx
@@ -1,12 +1,13 @@
+import Image from "next/image";
 
 export default function TheGuide() {
   return(
     <div className="full-width-container bg-[url(/experience.jpg)] bg-center md:bg-top bg-cover bg-fixed h-60 mt-10">
       <div className="bg-black/60 grow flex flex-col items-center justify-center gap-2">
-        <ul className="text-white text-2xl list-disc space-y-8">
-          <li>React Expertise</li>
-          <li>7 Years of .NET Experience</li>
-          <li>Built scalable solutions serving 70,000+ users</li>
+        <ul className="text-white text-2xl space-y-6 font-bold">
+          <li className="flex items-center"><Image width={48} height={48} src="/check.png" alt="Checkmark" />React Expertise</li>
+          <li className="flex items-center"><Image width={48} height={48} src="/check.png" alt="Checkmark" />7 Years of .NET Experience</li>
+          <li className="flex items-center"><Image width={48} height={48} src="/check.png" alt="Checkmark" />Built scalable solutions serving 70,000+ users</li>
         </ul>
       </div>
     </div>
