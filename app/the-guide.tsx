df04146395e8745a4a93d0c2f764b740c15ea249
Alex Johnston <me@alexj.ca>
2025-10-16 18:31:50 -0600

    Fix2

--- app/the-guide.tsx
+++ app/the-guide.tsx
@@ -5,10 +5,10 @@ export default function TheGuide() {
     <div className="bg-[url(/experience.jpg)] bg-center md:bg-top bg-cover bg-fixed h-60 mt-10">
       <div className="full-width-container bg-black/60 h-60">
         <div className="content-width-full flex flex-col justify-center">
-          <ul className="text-white text-2xl space-y-6 font-bold">
+          <ul className="text-white text-2xl font-bold sm:grid grid-cols-3">
             <li className="flex items-center"><Image width={48} height={48} src="/check.png" alt="Checkmark" />React Expertise</li>
             <li className="flex items-center"><Image width={48} height={48} src="/check.png" alt="Checkmark" />7 Years of .NET Experience</li>
-            <li className="flex items-center"><Image width={48} height={48} src="/check.png" alt="Checkmark" />Built scalable solutions serving 70,000+ users</li>
+            <li className="flex items-center"><Image width={48} height={48} src="/check.png" alt="Checkmark" />Solutions serving 70,000+ users</li>
           </ul>
         </div>
       </div>
