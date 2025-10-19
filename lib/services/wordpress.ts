ad8843da815907a58880573f1b53955de0fc1c20
Alex Johnston <me@alexj.ca>
2025-10-19 05:36:47 -0600

    fix 3

--- lib/services/wordpress.ts
+++ lib/services/wordpress.ts
@@ -6,7 +6,7 @@ const WP_API_URL = 'https://public-api.wordpress.com/rest/v1.1/sites/wp.joelj.ca
 export async function getPostsFromWordpress(page: number = 1, perPage: number = 5): Promise<Posts> {
   return await fetch(`${WP_API_URL}/posts?page=${page}&number=${perPage}`)
     .then(res => res.json().then(json => ({
-      posts: json.posts.map((post: any) => ({
+      posts: json.posts.map((post: Post) => ({
         ...post,
         title: post.title.replace('&#8217;', "'")
       })),
