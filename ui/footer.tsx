7ae4c32f166a731984902141db7e2e9fbec6cca7
Alex Johnston <me@alexj.ca>
2025-11-02 10:06:15 -0700

    Fix footer spacing

--- ui/footer.tsx
+++ ui/footer.tsx
@@ -10,7 +10,7 @@ const socialMediaItems = [
 export default function Footer() {
   return (
     <footer className="flex justify-center drop-shadow-md	bg-aqua-glow-700">
-      <div className="flex max-w-6xl grow my-8 items-start px-3 grid sm:grid-cols-2 lg:grid-cols-4 text-white">
+      <div className="max-w-6xl grow my-8 items-start px-3 grid gap-5 sm:grid-cols-2 lg:grid-cols-4 text-white">
         <div>
           <p>&copy; Midnight Sky Software Solutions Ltd.</p>
           <p>I was born to run~</p>
