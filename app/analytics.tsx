383de767600f86a64fab22685cc4a91c60d3975f
Alex Johnston <me@alexj.ca>
2025-11-05 06:24:25 -0700

    Restore google analytics

--- app/analytics.tsx
+++ app/analytics.tsx
@@ -7,7 +7,15 @@ import Script from "next/script";
 export default function Analytics() {
   return (
     <>
-      <Script strategy="lazyOnload" />
+      <Script strategy="lazyOnload">
+        window.dataLayer = window.dataLayer || [];
+        function gtag(){
+          (window as any).dataLayer.push(arguments)
+        }
+        gtag('js', new Date());
+
+        gtag('config', 'G-G2W02313YC');
+      </Script>
     </>
   );
 }
