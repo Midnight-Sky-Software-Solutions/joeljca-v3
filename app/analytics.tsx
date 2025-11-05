dcd55b7f24292b302fb8693de58c59ee7e3bb048
Alex Johnston <me@alexj.ca>
2025-11-05 06:14:12 -0700

    test

--- app/analytics.tsx
+++ app/analytics.tsx
@@ -1,6 +1,6 @@
 'use client'
 
-import { GoogleAnalytics } from "@next/third-parties/google";
+// import { GoogleAnalytics } from "@next/third-parties/google";
 import Script from "next/script";
 
 
