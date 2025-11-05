a2e85c5b5c2d9aecb680128f9a5090d8bc34adc7
Alex Johnston <me@alexj.ca>
2025-11-04 19:20:46 -0700

    Font display swap

--- ui/fonts.tsx
+++ ui/fonts.tsx
@@ -3,5 +3,6 @@ import { Marvel } from "next/font/google";
 export const marvel = Marvel({
   variable: "--font-marvel",
   subsets: ["latin"],
-  weight: ["400", "700"]
+  weight: ["400", "700"],
+  display: "swap"
 })
