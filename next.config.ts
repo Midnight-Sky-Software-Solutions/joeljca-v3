c8d94b99df7c135437faf1a524a0778db4165e95
Alex Johnston <me@alexj.ca>
2025-11-04 20:37:34 -0700

    nope

--- next.config.ts
+++ next.config.ts
@@ -2,12 +2,6 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   cacheComponents: true,
-  webpack: (config, { dev, isServer }) => {
-    if (!dev && !isServer) {
-      config.optimization.minimize = true;
-    }
-    return config
-  },
 };
 
 const withBundleAnalyzer = require('@next/bundle-analyzer')({
